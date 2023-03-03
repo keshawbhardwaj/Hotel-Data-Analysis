faf8aff7d2f2c0a16941f4336e42faf66834fee9
Keshaw Bhardwaj <119773619+keshawbhardwaj@users.noreply.github.com>
2023-03-03 12:59:12 +0530

    Update README.sql

--- README.sql
+++ README.sql
@@ -1,5 +1,7 @@
 --# Hotel-Data-Analysis
 --This beginner-friendly hotel data analysis project will build a database with SQL, analyze data, connect to Power BI, and create interactive visualizations. Following best practices and open-source tools, deliverables will include a database, SQL queries, and Power BI reports, valuable for a data analyst's portfolio.
+
+
 select  * from dbo.['2019$']
 
 union
